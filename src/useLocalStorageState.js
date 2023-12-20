ea4270aae47767a5e672d2cf82231f6df9b01ad6
Ratul <shahoriar.ratul@gmail.com>
2023-12-20 11:55:48 +0600

    fixed

--- src/useLocalStorageState.js
+++ src/useLocalStorageState.js
@@ -1,14 +1,17 @@
-import { useEffect, useState } from "react";
-export function useLocalStorageState(initialSate, key) {
+import { useState, useEffect } from "react";
+
+export function useLocalStorageState(initialState, key) {
   const [value, setValue] = useState(function () {
-    const storeValue = localStorage.getItem(key);
-    return storeValue ? JSON.parse(storeValue) : initialSate;
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialState;
   });
+
   useEffect(
     function () {
-      localStorage.setItem("watched", JSON.stringify(value));
+      localStorage.setItem(key, JSON.stringify(value));
     },
     [value, key]
   );
+
   return [value, setValue];
 }
