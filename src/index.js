ae86eb7293593db7053b6301e4c96e9a9d7df906
Shahoriar Ratul <36799076+Shahoriarratul@users.noreply.github.com>
2023-12-20 11:46:53 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,28 +4,28 @@ import "./index.css";
 import App from "./App";
 import StarRating from "./StarRating";
 
-// function HandleRating() {
-//   const [autoRate, setAutoRate] = useState(0);
-//   return (
-//     <div>
-//       <StarRating color="blue" maxRating={5} />
-//       <p>Rating: {autoRate}</p>
-//       <StarRating
-//         maxRating={10}
-//         color="red"
-//         className="test"
-//         defaultRating={3}
-//         onSetRating={setAutoRate}
-//       />
-//     </div>
-//   );
-// }
+function HandleRating() {
+  const [autoRate, setAutoRate] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={5} />
+      <p>Rating: {autoRate}</p>
+      <StarRating
+        maxRating={10}
+        color="red"
+        className="test"
+        defaultRating={3}
+        onSetRating={setAutoRate}
+      />
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
 
-    {/* <HandleRating /> */}
+     <HandleRating /> 
   </React.StrictMode>
 );
